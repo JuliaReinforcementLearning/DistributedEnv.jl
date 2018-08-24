ac0a7f0fd131cd9a7e6902e6793bda1cde69db14
Jun Tian <find_my_way@foxmail.com>
2018-08-24 13:59:10 +0800

    remove redundant codes

--- src/DistributedEnv.jl
+++ src/DistributedEnv.jl
@@ -2,11 +2,8 @@ module DistributedEnv
 export init_env
 
 include("Env/Env.jl")
-
 include("abstractenv.jl")
 include("denv.jl")
-
-@everywhere include(joinpath(@__DIR__, "Env", "Env.jl"))
 
 function init_env(id::String, n::Int=1, workers::Vector{Int}=workers())
     [DEnv(id, wid) for wid in workers for _ in 1:n]
