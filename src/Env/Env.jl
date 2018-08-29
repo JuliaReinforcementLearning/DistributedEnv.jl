c401f7208a62424f30695d243ac2d3a5e895d439
Jun Tian <find_my_way@foxmail.com>
2018-08-29 13:41:22 +0800

    register vizdoom

--- src/Env/Env.jl
+++ src/Env/Env.jl
@@ -6,9 +6,11 @@ include("Space/Space.jl")
 include("abstractenv.jl")
 include("pygymenv.jl")
 include("ale.jl")
+include("vizdoom.jl")
 
 const supportedIDs = merge(Dict("pygym#" * x => GymEnv for x in pygym_ids),
-                           Dict("atari#" * x => AtariEnv for x in atari_ids))
+                           Dict("atari#" * x => AtariEnv for x in atari_ids),
+                           Dict("vizdoom#basic" => ViZDoomEnv))
 
 function id2env(id::String; kw...)
     haskey(supportedIDs, id) || throw("$id is not supported yet!")
